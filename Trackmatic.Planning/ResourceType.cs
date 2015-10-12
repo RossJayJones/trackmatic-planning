cb2d48f8cdc300213e60f9d98d2e726694748dae
howie <howards@trackmatic.co.za>
2015-10-12 13:25:59 +0200

    Committed Position & ResourceType classes

---
+++ Trackmatic.Planning/ResourceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trackmatic.Planning
+{
+    public class ResourceType
+    {
+        public string Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public List<Resource> Resources { get; set; }
+    }
+}
